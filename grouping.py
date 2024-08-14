52c1db68a3429888bf0575047564db5c4ee99c68
Naman Joshi <Joshinaman09@gmail.com>
2024-08-14 11:57:38 +0530

    group-by added

---
+++ grouping.py
@@ -0,0 +1,18 @@
+# importing pandas module
+import pandas as pd 
+ 
+# Define a dictionary containing employee data 
+data1 = {'Name':['Jai', 'Anuj', 'Jai', 'Princi', 
+                 'Gaurav', 'Anuj', 'Princi', 'Abhi'], 
+        'Age':[27, 24, 22, 32, 
+               33, 36, 27, 32], 
+        'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj',
+                   'Jaunpur', 'Kanpur', 'Allahabad', 'Aligarh'], 
+        'Qualification':['Msc', 'MA', 'MCA', 'Phd',
+                         'B.Tech', 'B.com', 'Msc', 'MA']} 
+   
+ 
+# Convert the dictionary into DataFrame  
+df = pd.DataFrame(data1)
+ 
+print(df) 
